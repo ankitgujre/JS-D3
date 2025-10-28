2316c10488a4d316446a73abbbdcb5d7bef042d5
ankitgujre <ankitgujre374@gmail.com>
2025-10-28 16:34:28 +0530

    data types

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 a = 12*"abc"
 
 console.log(a)   //yah Nan Dega console me
+
+// infinity
+
+console.log(4*0)
+
+// refrences = array object functions
+
+var a = [1,2,3,4,5]
+var b = a;
+
+console.log(b.pop(1))
+
+// data types
+/*
+float 12.3
+number
+string
+boolean = true aur false
+null = jab apne pass koi value na ho dene ke liye tab use hota hai
+array = ek se jyada value hold karne ke liye
+object
+symbol
+undefined
+NaN = koi number ke saath aisa operation kiya jo ho nahi sakta
+*/
+
+let name = "Ankit";
+let age = 25;
+let isStudent = true;
+
+console.log(typeof name);      // "string"
+console.log(typeof age);       // "number"
+console.log(typeof isStudent); // "boolean"
